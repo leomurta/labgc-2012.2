fbda17cb9207273a0b657f772164e28c074758b6
Felipe Duval <jokerfvd@gmail.com>
2012-08-30 14:24:11 -0300

    adicionando pacotes storage e versioning

---
+++ src/main/java/storage/IStorage.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package storage;
+
+/**
+ *
+ * @author jokerfvd
+ */
+public interface IStorage {
+    
+}
