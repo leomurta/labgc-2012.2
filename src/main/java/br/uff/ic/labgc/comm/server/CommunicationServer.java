6a25fec6c7b59ee9c3b854f5603e5201e879cf75
Cristiano Cesário <cesario.cristiano@gmail.com>
2012-09-01 08:53:21 -0300

    Inclusão de classes relacionadas à comunicação, fábrica e carga de propriedades.

---
+++ src/main/java/br/uff/ic/labgc/comm/server/CommunicationServer.java
@@ -0,0 +1,37 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uff.ic.labgc.comm.server;
+
+import br.uff.ic.labgc.server.IServer;
+import java.io.File;
+import java.util.List;
+
+/**
+ *
+ * @author Cristiano
+ */
+public class CommunicationServer implements IServer {
+
+    public boolean commit(List<File> file, String message) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<File> update() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public List<File> checkout() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String diff(File file, String version) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public String log() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
---
+++ src/main/java/br/uff/ic/labgc/properties/IPropertiesConstants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.uff.ic.labgc.properties;
+
+/**
+ * Armazena as constantes relacionadas ao arquivo de propriedades.
+ * @author Cristiano
+ */
+public interface IPropertiesConstants {
+    public static String PROPERTIES_FILE_NAME = "br.uff.ic.labgc.properties.labgc";
+    public static String SERVER_CLASS = "serverclass";
+    
+}
