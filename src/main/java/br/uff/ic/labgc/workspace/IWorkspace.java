3fadf017f6c86e4f6d176933d9f369daac2d8d15
vyctorh <vyctorh@hotmail.com.br>
2012-09-04 20:30:36 -0300

    Adicionandoprocedures

--- src/main/java/br/uff/ic/labgc/workspace/IWorkspace.java
+++ src/main/java/br/uff/ic/labgc/workspace/IWorkspace.java
@@ -4,10 +4,59 @@
  */
 package br.uff.ic.labgc.workspace;
 
+import java.io.File;
+
 /**
  *
  * @author vyctorh
  */
-public interface IWorkspace {
+public class IWorkspace {
+    
+boolean remove(File file) {
+        return false;
+   
+    }
+
+boolean move(File file, String dest) {
+        return false;
+        
+    }
+
+boolean copy(File file, String dest) {
+        return false;
+ 
+    }
+
+boolean mkdir(String name) {
+        return false;
+ 
+    }
+    
+    //comandos de diretorio
+    
+boolean add(File file) {
+        return false;
+ 
+    }
+
+boolean revert(File file) {
+        return false;
+ 
+    }
+
+String status() {
+        return null;
+  
+    }
+
+boolean release() {
+        return false;
+
+    }
+
+boolean resolve(File file) {
+        return false;
+
+    }
     
 }
