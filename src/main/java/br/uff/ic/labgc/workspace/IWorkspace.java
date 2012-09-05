6f14dc54200821a61f932ee72343b284255753ca
vyctorh <vhugo@centroin.com.br>
2012-09-05 17:42:56 -0300

    adicionando nova versão da workspace com as chamadas do cliente

--- src/main/java/br/uff/ic/labgc/workspace/IWorkspace.java
+++ src/main/java/br/uff/ic/labgc/workspace/IWorkspace.java
@@ -4,10 +4,89 @@
  */
 package br.uff.ic.labgc.workspace;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+
 /**
  *
  * @author vyctorh
  */
-public interface IWorkspace {
+public class IWorkspace {
     
+    /**
+     *
+     * @param file
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public boolean remove(File file) 
+    throws FileNotFoundException, IOException {
+
+        return false;
+   
+    }
+
+
+    /**
+     *
+     * @param file
+     * @param dest
+     * @return
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    public boolean move(File file, String dest) 
+    throws FileNotFoundException, IOException  
+{
+
+        return false;
+        
+    }
+
+
+    boolean copy(File file, String dest)
+    throws FileNotFoundException, IOException  {
+
+        return false;
+ 
+    }
+
+
+    boolean mkdir(String name)
+    throws IOException  {
+        return false;
+
+ 
+    }
+    
+    //comandos de diretorio
+    
+    boolean add(File file)
+    throws IOException  {
+        return false;
+ 
+    }
+
+boolean revert(File file) {
+        return false;
+ 
+    }
+
+String status() {
+        return null;
+  
+    }
+
+boolean release() {
+        return false;
+
+    }
+
+boolean resolve(File file) {
+        return false;
+
+    }   
 }
