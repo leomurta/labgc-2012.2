9d6879e16b01ed1d2d77a22e18deb0d5f1773774
vyctorh <vyctorh@hotmail.com>
2012-09-16 20:03:29 -0300

    Incluido criarWorspace e usando mensagens do projeto

--- src/main/java/br/uff/ic/labgc/workspace/Workspace.java
+++ src/main/java/br/uff/ic/labgc/workspace/Workspace.java
@@ -4,9 +4,12 @@
 */
 package br.uff.ic.labgc.workspace;
 
+import br.uff.ic.labgc.exception.WorkspaceException;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 
 /**
@@ -90,4 +93,33 @@ boolean resolve(File file) {
         return false;
 
     }
+// diretorio = diretorio raiz do projeto e raiz=caminho da raiz do repositorio e repositorio=caminho dentro da raiz
+public static void criaWorkSpace(File diretorio, String Raiz, String Repositorio)
+throws IOException, WorkspaceException {
+    if (!diretorio.exists()) {
+        diretorio.mkdirs();
+    }
+    else{
+        throw new WorkspaceException ("ERRO: Diretório existente.");
+    }
+       	File vcs = new File (diretorio, "vcs");
+	if (!vcs.exists()) {
+        vcs.mkdir();
+    }
+                
+        File entrada = new File (vcs, "Entrada");
+	entrada.createNewFile();
+                        
+	File raiz = new File (vcs, "raiz");
+	raiz.createNewFile();
+        FileWriter raizarq = new FileWriter(raiz, false);
+	PrintWriter out = new PrintWriter(raizarq);
+	out.println(Raiz);
+	out.close ();
+                
+	out = new PrintWriter(new FileWriter(new File (vcs, "repositorio")));
+	out.println(Repositorio);
+	out.close ();
+}
+
 }
