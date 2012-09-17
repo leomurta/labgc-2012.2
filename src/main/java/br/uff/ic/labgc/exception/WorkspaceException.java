8c9e29e7e89c876809888d23f290902cf5096de0
vyctorh <vyctorh@hotmail.com>
2012-09-16 23:26:13 -0300

    Incluido mensagens do projeto

--- src/main/java/br/uff/ic/labgc/exception/WorkspaceException.java
+++ src/main/java/br/uff/ic/labgc/exception/WorkspaceException.java
@@ -8,4 +8,12 @@ package br.uff.ic.labgc.exception;
 public class WorkspaceException extends ApplicationException {
     private static final long serialVersionUID = 7280993250981760795L;
     
+    public WorkspaceException(String message, Exception ex) {
+    super(message, ex);
+    }
+    
+    public WorkspaceException(String message) {
+    super(message);
+    }
+
 }
