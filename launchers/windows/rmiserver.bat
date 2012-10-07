822e52969a0fe75ed9f45a6252144710daab7ee9
Felipe R <fralph@ic.uff.br>
2012-10-07 12:01:47 -0300

    implementado pedido do jdk path no rmiserver.bat para a criacao do arquivo jdk~path.txt customizado para cada membro da equipe

--- launchers/windows/rmiserver.bat
+++ launchers/windows/rmiserver.bat
@@ -1,18 +1,73 @@
 @ECHO off
-setlocal
 ::# Levantando o RMI
 ::
-::Entrar com o caminho completo da pasta do jdk
+::Entrar com o caminho completo da pasta do jdk no aruqivo jdk_path.txt
 ::
-set LABGC_JDK_PATH=C:\Program Files\Java\jdk1.7.0_03
 
-::programa
-set LABCG_OUT=%LABGC_JDK_PATH%\bin\rmiregistry
-echo %LABCG_OUT%
+set _LABGC_FILE=jdk~path.txt
+set _LABGC_RMI_BIN=\bin\rmiregistry.exe
+
+set _LABGC_JDK_PATH=jdk
+
+call :TXTCHECK %_LABGC_FILE%
+EXIT /B
+
+
+REM =======================================================================================
+REM =   Check txt location
+REM =======================================================================================
+:TXTCHECK
+if exist "%~1" (
+	for /F "delims=" %%a in (%~1) do (call :RMICHECK "%%a")
+) else (
+    call :ENTRYJDK
+)
+EXIT /B
+
+
+REM =======================================================================================
+REM =   Check RMISEVER location
+REM =======================================================================================
+:RMICHECK
+if exist %~1 (
+	call :EXECUTE "%~1"
+) else (
+	call :ENTRYJDK
+)
+EXIT /B
+
+
+REM =======================================================================================
+REM =   Execute RMISERVER
+REM =======================================================================================
+:EXECUTE
+echo "%~s1"
 echo Executando...
 echo Ctrl+C para encerrar o RMIREGISTRY server
+"%~s1"
+EXIT
 
-"%LABCG_OUT%"
 
-endlocal
+REM =======================================================================================
+REM =   Entry JDK Location
+REM =======================================================================================
+:ENTRYJDK
+echo .
+echo Entre com o caminho do JDK" 
+set /p LABCG_JDK=JDK:
+call :WRITEFILE "%LABCG_JDK%%_LABGC_RMI_BIN%"
+EXIT /B
 
+
+REM =======================================================================================
+REM =   Write text file
+REM =======================================================================================
+:WRITEFILE
+if exist "%~S1" (
+	if exist "%_LABGC_FILE%" (del %_LABGC_FILE%)
+    ECHO %~S1 >> %_LABGC_FILE%
+	echo arquivo criado!
+) else (
+    echo Nao foi possivel encontrar o arquivo no local especificado! Rode o comando novamente!
+)
+EXIT /B
