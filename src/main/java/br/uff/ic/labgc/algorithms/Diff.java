9d0abd048939fc767526f4fca9343a82fac39641
Raphael Evangelista <rapharj1990@gmail.com>
2012-10-14 21:22:49 -0300

    LCS Finalizado
    - Ainda não testado !

--- src/main/java/br/uff/ic/labgc/algorithms/Diff.java
+++ src/main/java/br/uff/ic/labgc/algorithms/Diff.java
@@ -13,7 +13,11 @@ import java.io.File;
  */
 public class Diff {
     
-    public static byte[] run( VersionedItem file1, VersionedItem file2 ){
+    // OUTPUT DELTA
+    // <- Não definido ainda ! ->
+    //
+    
+    public static byte[] run( VersionedItem file1, VersionedItem file2 ) throws ApplicationException, IncompatibleItensException{
         
         if( isFile(file1) && isFile(file2) ){
             return diff_archives( (VersionedFile)file1, (VersionedFile)file2 );
@@ -31,12 +35,22 @@ public class Diff {
         // Chama LCS
         // 
         
-        byte[] final_file;
+        byte[] final_file = null;
         
-        // Registra informações dos arquivos que estão sendo diferenciados
+        // Registra METADADOS dos arquivos que estão sendo diferenciados
         
         if( file1.getHash() != file2.getHash() ){
+            
             byte[] lcs_file = Diff.lcs( file1.getContent(), file2.getContent() );
+            int num_of_blocks = countLines(lcs_file);
+            
+            addBlock( lcs_file, final_file );
+            
+            for( int i = 0; i < num_of_blocks + 1; i++ ){
+                byte[] block = identifyBlock( file1, lcs_file, i );
+                addBlock( block, final_file );
+            }
+            
         }
         return null;
     }
@@ -45,6 +59,7 @@ public class Diff {
         return null;
     }
     
+    // Finalizado
     private static boolean isFile( VersionedItem file1 ){
         try{
             VersionedFile aux = (VersionedFile) file1;
@@ -54,9 +69,8 @@ public class Diff {
         return true;
     }
     
+    // Finalizado
     private static byte[] lcs( byte[] seq1, byte[] seq2 ){
-        // Definir tamanho de "seq1" como sendo o número de linhas (qtd. de \n)
-        // Definir tamanho de "seq2" como sendo o número de linhas (qtd. de \n)
         
         byte[] fline_seq1 = getFline(seq1); // Identifica primeira linha seq1
         byte[] fline_seq2 = getFline(seq2); // Identifica segunda  linha seq2
@@ -66,18 +80,23 @@ public class Diff {
         
         if( hasDiff( fline_seq1, fline_seq2 ) ){            
             // Chamar duas frentes de LCS
-            lcs( new_seq1, seq2 );
-            lcs( seq1, new_seq2 );
+            byte[] lcs1 = lcs( new_seq1, seq2 );
+            byte[] lcs2 = lcs( seq1, new_seq2 );
             
             // Verifica qual das duas frentes possui maior sequência comum
             // Retorna
+            if( countLines(lcs1) >= countLines(lcs2) ){
+                return lcs1;
+            } else {
+                return lcs2;
+            }
         } else {
             return addFline( fline_seq1, lcs( new_seq1, new_seq2 ) );
         }
-        
-        return null;
+
     }
 
+    // Finalizado
     private static boolean hasDiff(byte[] fline_seq1, byte[] fline_seq2) {
         for( int i = 0; i < fline_seq1.length; i++ ){
             if( fline_seq1[i] != fline_seq2[i] ){
@@ -96,6 +115,18 @@ public class Diff {
     }
 
     private static byte[] addFline( byte[] fline_seq1, byte[] seq ) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private static int countLines(byte[] lcs_file) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private static void addBlock(byte[] lcs_file, byte[] final_file) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    private static byte[] identifyBlock(VersionedFile file1, byte[] lcs_file, int i) {
         throw new UnsupportedOperationException("Not yet implemented");
     }
     
