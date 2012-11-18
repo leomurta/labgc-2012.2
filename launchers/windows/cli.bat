0a3ae52701026f3e057689e84774af22637ea075
Felipe R <fralph@ic.uff.br>
2012-11-18 21:48:59 -0200

    ajuste dos bats do windows com a nova propriedade(repository)

--- launchers/windows/cli.bat
+++ launchers/windows/cli.bat
@@ -7,6 +7,7 @@ set LABCG_PATH="../../target/labgc-2012.2.jar"
 set LABCG_RMI_CODEBASE="../../target/classes/"
 set LABCG_RMI_HOSTNAME=localhost
 set LABCG_POLICITY="../../target/classes/evcs.policy"
+set LABCG_REPOSITORY="../../repositorio/"
 ::
 ::
 
@@ -14,8 +15,9 @@ set LABCG_POLICITY="../../target/classes/evcs.policy"
 for %%i in (%LABCG_PATH%) do (set LABCG_PATH=%%~si)
 for %%i in (%LABCG_RMI_CODEBASE%) do (set LABCG_RMI_CODEBASE=%%~si)
 for %%i in (%LABCG_POLICITY%) do (set LABCG_POLICITY=%%~si)
+for %%i in (%LABCG_REPOSITORY%) do (set LABCG_REPOSITORY=%%~si)
 
-set LABCG_OUT="java -cp %LABCG_PATH% -Dbr.uff.ic.labgc.invocation=%~sdp0 -Djava.rmi.server.codebase=file:\%LABCG_RMI_CODEBASE% -Djava.rmi.server.hostname=%LABCG_RMI_HOSTNAME% -Djava.security.policy=%LABCG_POLICITY% br.uff.ic.labgc.AppCLI %*"
+set LABCG_OUT="java -cp %LABCG_PATH% -Dbr.uff.ic.labgc.invocation=%~sdp0 -Dbr.uff.ic.labgc.repository=%LABCG_REPOSITORY% -Djava.rmi.server.codebase=file:\%LABCG_RMI_CODEBASE% -Djava.rmi.server.hostname=%LABCG_RMI_HOSTNAME% -Djava.security.policy=%LABCG_POLICITY% br.uff.ic.labgc.AppCLI %*"
 echo %LABCG_OUT%
 
 echo Executando...
